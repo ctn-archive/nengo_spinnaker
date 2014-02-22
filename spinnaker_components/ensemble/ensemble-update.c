94b368ff89c79559c5e53cac33aa8e1c46b6e7cd
AndrewMundy <andrew.mundy@ieee.org>
2014-02-21 20:48:14 -0500

    Adding some copyright and contact information.

--- spinnaker_components/ensemble/ensemble-update.c
+++ spinnaker_components/ensemble/ensemble-update.c
@@ -1,3 +1,27 @@
+/*****************************************************************************
+
+SpiNNaker and Nengo Integration
+
+******************************************************************************
+
+Authors:
+ Andrew Mundy <mundya@cs.man.ac.uk> -- University of Manchester
+ Terry Stewart			    -- University of Waterloo
+
+Date:
+ 17-22 February 2014
+
+******************************************************************************
+
+Advanced Processors Technologies,   Computational Neuroscience Research Group,
+School of Computer Science,         Centre for Theoretical Neuroscience,
+University of Manchester,           University of Waterloo,
+Oxford Road,                        200 University Avenue West,
+Manchester, M13 9PL,                Waterloo, ON, N2L 3G1,
+United Kingdom                      Canada
+
+*****************************************************************************/
+
 #include "spin-nengo-ensemble.h"
 
 void timer_callback( uint arg0, uint arg1 )
