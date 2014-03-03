53203d62ec2561d02e80aa264642f1ba2e02303b
Andrew Mundy <andrew.mundy@ieee.org>
2014-03-03 13:09:53 +0000

    Added SDP Receipt capability.

--- spinnaker_components/rx/rx-sdp.c
+++ spinnaker_components/rx/rx-sdp.c
@@ -25,5 +25,12 @@ United Kingdom                      Canada
 #include "rx.h"
 
 void sdp_packet_received( uint mailbox, uint port ) {
-  // Process received SDP packet
+  sdp_msg_t *msg = ( sdp_msg_t * ) mailbox;
+
+  switch ( msg->cmd_rc ) {
+    case 0x00000001:  // Replace dimensional values - TODO: Change this?
+      spin1_memcpy( msg->data, values, n_dimensions * sizeof( value_t ) );
+      spin1_msg_free( msg );
+      break;
+  };
 }
