8920b64c0675e1c6933de81f74b342eab56c3824
Andrew Mundy <andrew.mundy@ieee.org>
2014-03-05 11:34:23 +0000

    Use SDP packet arguments to control which values are replaced.

--- spinnaker_components/rx/rx-sdp.c
+++ spinnaker_components/rx/rx-sdp.c
@@ -17,7 +17,13 @@ void sdp_packet_received( uint mailbox, uint port ) {
 
   switch ( msg->cmd_rc ) {
     case 0x00000001:  // Replace dimensional values - TODO: Change this?
-      spin1_memcpy( msg->data, values, n_dimensions * sizeof( value_t ) );
+      // arg1: First dimension to update
+      // arg2: Number of dimensions to update
+      spin1_memcpy(
+        msg->data,
+        &values[ msg->arg1 ],
+        msg->arg2 * sizeof( value_t )
+      );
       spin1_msg_free( msg );
       break;
   };
