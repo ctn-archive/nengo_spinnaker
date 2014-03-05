f7fcb0c0124fb021512f7fff2a6dc904880c2e95
Andrew Mundy <andrew.mundy@ieee.org>
2014-03-05 11:22:48 +0000

    Actually transmit some information.

--- examples/communication.py
+++ examples/communication.py
@@ -1,18 +1,18 @@
 import nengo
 import numpy as np
 
-D = 2
+D = 10
 
 model = nengo.Model('Communication Channel')
 with model:
-    #input = nengo.Node(np.sin, label='input')
+    input = nengo.Node(np.sin, label='input')
     #a = nengo.Ensemble(100, D, label='a')
     b = nengo.Ensemble(90, D, label='b')
     def printout(t, x):
         print t, x
     output = nengo.Node(printout, size_in=D, label='output')
     
-    #nengo.Connection(input, a, filter=0.01, transform=[[1]]*D)
+    nengo.Connection(input, b, filter=0.01, transform=[[1]]*D)
     #nengo.Connection(a, b, filter=0.01)
     #nengo.Connection(b, b, transform=0.9, filter=0.1)
     nengo.Connection(b, output, filter=0.01)
