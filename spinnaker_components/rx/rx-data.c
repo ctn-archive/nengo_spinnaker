015a7de08c8ac328efe07a3b59d5e184293fcd69
Andrew Mundy <andrew.mundy@ieee.org>
2014-03-05 10:23:23 +0000

    Some documentation for the Rx component, this will need some reworking to play nicely with the rest of the documentation.

--- spinnaker_components/rx/rx-data.c
+++ spinnaker_components/rx/rx-data.c
@@ -1,32 +1,32 @@
-/*****************************************************************************
-
-SpiNNaker and Nengo Integration
-
-******************************************************************************
-
-Authors:
- Andrew Mundy <mundya@cs.man.ac.uk> -- University of Manchester
- Terry Stewart			    -- University of Waterloo
-
-Date:
- 17-22 February, 3 March 2014
-
-******************************************************************************
-
-Advanced Processors Technologies,   Computational Neuroscience Research Group,
-School of Computer Science,         Centre for Theoretical Neuroscience,
-University of Manchester,           University of Waterloo,
-Oxford Road,                        200 University Avenue West,
-Manchester, M13 9PL,                Waterloo, ON, N2L 3G1,
-United Kingdom                      Canada
-
-*****************************************************************************/
+/*
+ * Authors:
+ *   - Andrew Mundy <mundya@cs.man.ac.uk>
+ *   - Terry Stewart
+ * 
+ * Copyright:
+ *   - Advanced Processor Technologies, School of Computer Science,
+ *      University of Manchester
+ *   - Computational Neuroscience Research Group, Centre for
+ *      Theoretical Neuroscience, University of Waterloo
+ * 
+ * \addtogroup rxcomponent
+ * @{
+ */
 
 #include "rx.h"
 
 uint n_dimensions, dt, ticks_per_output, n_current_output, *keys;
 value_t *values;
 
+/**
+ * \brief Copy in system region data.
+ * 
+ * Value (Type) | Description
+ * ------------ | -----------
+ * n_dimensions (```uint```) | Number of output dimensions <= 64
+ * dt (```uint```) | Time period of simulation in microseconds
+ * 
+ */
 void copy_in_system_region( address_t addr ) {
   n_dimensions = addr[0]; // Number of dimensions to represent
   dt = addr[1];           // Time step in us
@@ -42,10 +42,33 @@ void copy_in_system_region( address_t addr ) {
   values = spin1_malloc( sizeof( value_t ) * n_dimensions );
 }
 
+/**
+ * \brief Copy in routing keys data
+ *
+ * Complete routing keys (```uint```) to be used when transmitting 
+ * dimensional data.  These are normally formed as:
+ * 
+ * ```( x << 24 ) | ( y << 16 ) | ( (p-1) << 11 ) | ( i << 6 ) | d```
+ * 
+ * Where ```x```, ```y``` and ```p``` refer to the processor where the Rx
+ * component resides; ```i``` refers to the index of the connection/edge the data
+ * is associated with (e.g., a given Rx component may feed multiple sinks with
+ * different sets of dimensions) and ```d``` refers to the specific dimension
+ * being transmitted.
+ */
 void copy_in_keys( address_t addr ) {
   spin1_memcpy( keys, addr, n_dimensions * sizeof( uint ) );
 }
 
+/**
+ * \brief Copy in initial values data
+ *
+ * The initial values to transmit (as there may be some lag between the start of
+ * the simulation and the first SDP packets arriving).  These must be in the same
+ * order as the routing keys.
+ */
 void copy_in_initial_values( address_t addr ) {
   spin1_memcpy( values, addr, n_dimensions * sizeof( value_t ) );
 }
+
+/** @} */
