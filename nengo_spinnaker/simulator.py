415ec913597be263e4ccc056c50615a2b89cb0db
Andrew Mundy <andrew.mundy@ieee.org>
2014-04-29 11:15:35 +0100

    Bring `Simulator` in-line with earlier commits.
     - Remove serial I/O functionality (moved to relevant Communicator class)
     - Create an Ethernet connection by default
     - Added `NodeSimulator` to periodically simulate Nodes
     - Functionality to instatiate IO, Node Simulators and Simulate!

--- nengo_spinnaker/simulator.py
+++ nengo_spinnaker/simulator.py
@@ -1,11 +1,15 @@
 import sys
+import logging
+import threading
+import time
 
 from pacman103.core import control
 from pacman103 import conf
-from pacman103.lib import parameters
 
 from . import builder
 from . import node_builders
+
+logger = logging.getLogger(__name__)
 
 
 class Simulator(object):
@@ -13,16 +17,18 @@ class Simulator(object):
         # Build the model
         self.builder = builder.Builder()
 
+        # Set up the IO
+        self.machinename = conf.config.get('Machine', 'machineName')
         if io is None:
-            # TODO Get IP address of machine from perspective of SpiNNaker
-            # board
-            # io = io_builders.Ethernet(addr)
-            raise NotImplementedError
+            io = node_builders.Ethernet(self.machinename)
+        self.io = io
 
-        self.dao = self.builder(model, dt, seed, io_builder=io)
+        self.dao = self.builder(model, dt, seed, node_builder=io)
         self.dao.writeTextSpecs = True
 
-    def run(self, time):
+        self.dt = dt
+
+    def run(self, time_in_seconds=None):
         """Run the model, currently ignores the time."""
         self.controller = control.Controller(
             sys.modules[__name__],
@@ -30,125 +36,94 @@ class Simulator(object):
         )
 
         # Preparation functions
-        # Consider moving this into PACMAN103
         for vertex in self.dao.vertices:
             if hasattr(vertex, 'prepare_vertex'):
                 vertex.prepare_vertex()
 
         self.controller.dao = self.dao
-        self.dao.set_hostname(conf.config.get('Machine', 'machineName'))
+        self.dao.set_hostname(self.machinename)
+        self.dao.run_time = None  # TODO: Modify Transceiver so that we can
+                                  # manually check for application termination
+                                  # i.e., we want to do something during the
+                                  # simulation time, not pause in the TxRx.
+
+        # TODO: All of the following will become more modular!
         self.controller.map_model()
-        if self.builder.use_serial:
-            self.generate_serial_key_maps()
         self.controller.generate_output()
         self.controller.load_targets()
         self.controller.load_write_mem()
         self.controller.run(self.dao.app_id)
 
-        if self.builder.use_serial:
-            self.start_serial_io(time)
+        # Start the IO and perform host computation
+        with self.io as node_io:
+            # Create the Node threads
+            node_sims = [NodeSimulator(node, node_io, self.dt, time_in_seconds)
+                         for node in self.io.nodes]
 
-    def generate_serial_key_maps(self):
-        self.serial_rx = {}
-        self.serial_tx = {}
-        for edge in self.builder.serial.in_edges:
-            for subedge in edge.subedges:
-                key = edge.prevertex.generate_routing_info(subedge)[0]
-                node = edge.post
-                self.serial_tx[key] = node
-
-        for edge in self.builder.serial.out_edges:
-            for subedge in edge.subedges:
-                key = edge.prevertex.generate_routing_info(subedge)[0]
-                node = edge.pre
-                if node not in self.serial_rx:
-                    self.serial_rx[node] = [key]
+            # Sleep for simulation time/forever
+            try:
+                if time_in_seconds is not None:
+                    time.sleep(time_in_seconds)
                 else:
-                    self.serial_rx[node].append(key)
-
-    def send_inputs(self, t):
-        for node, keys in self.serial_rx.iteritems():
-            value = parameters.s1615(node.output(t))
-            for key in keys:
-                for d, v in enumerate(value):
-                    if v<0:
-                        v += 0x100000000
-                    msg = '%08x.%08x\n' % (key | d, v)
-                    self.serial.write(msg)
-        self.serial.flush()
-
-        #TODO: handle node to node and nodes that are both inputs and outputs
+                    while True:
+                        time.sleep(10.)
+            except KeyboardInterrupt:
+                pass
+            finally:
+                # Any necessary teardown functions
+                for sim in node_sims:
+                    sim.stop()
 
 
-    def handle_retina(self, x, y):
-        print 'retina', x, y
+class NodeSimulator(object):
+    """A "thread" to periodically evaluate a Node."""
+    def __init__(self, node, io, dt, time_in_seconds):
+        """Create a new NodeSimulator
 
+        :param node: the `Node` to simulate
+        :param io: the IO handler, providing functions `get_node_input` and
+                   `set_node_output`
+        :param dt: timestep with which to evaluate the `Node`
+        :param time_in_seconds: duration of the simulation
+        """
+        self.node = node
+        self.io = io
+        self.dt = dt
+        self.time = time_in_seconds
+        self.time_passed = 0.
 
-    def start_serial_io(self, time):
-        import serial
-        import time
+        self.timer = threading.Timer(self.dt, self.tick)
+        self.timer.start()
 
-        input_period = 0.1
-        last_input = None
+    def stop(self):
+        """Permanently stop simulation of the Node."""
+        self.time = 0.
+        self.timer.cancel()
 
-        self.serial = serial.Serial('/dev/ttyUSB0', baudrate=8000000,
-                                    rtscts=True)
-        self.serial.write("S+\n")   # send spinnaker packets to host
-        #self.serial.write("E+\n")   # send retina packets to host
-        #self.serial.write("Z+\n")   # turn on retina
-        start = time.time()
-        buffer = {}
-        line = ''
-        retina = False
-        while True:
-            now = time.time()
-            if last_input is None or now > last_input + input_period:
-                self.send_inputs(now - start)
+    def tick(self):
+        """Simulate the Node and prepare the next timer tick if necessary."""
+        start = time.clock()
 
-            c = self.serial.read()
-            assert len(c) == 1
-            line += c
+        node_output = None
 
-            if retina and len(line) >= 2:
-                self.handle_retina(ord(line[-2]), ord(line[-1]))
-                line = ''
-                retina = False
-                continue
+        if self.node.size_in > 0:
+            node_input = self.io.get_node_input(self.node)
 
-            if ord(c) & 0x80:
-                retina = True
-                line = c
-                continue
+            if node_input is not None and None not in node_input:
+                node_output = self.node.output(self.time_passed, node_input)
+        else:
+            node_output = self.node.output(self.time_passed)
 
-            if c == '\n':
-                if ord(line[0])==0:
-                    line = line[1:]
-                print line
-                if '.' not in line:
-                    continue
-                parts = line.split('.')
-                parts = [int(p,16) for p in parts]
-                if len(parts) == 3:
-                    header, key, payload = parts
+            if node_output is not None:
+                self.io.set_node_output(self.node, node_output)
+        stop = time.clock()
 
-                    if payload & 0x80000000:
-                        payload -= 0x100000000
-                    value = (payload * 1.0) / (2**15)
+        if stop - start > self.dt:
+            self.dt = stop - start
+            logger.warning("%s took longer than one timestep to simulate. "
+                           "Decreasing frequency of evaluation." % self.node)
 
-                    base_key = key & 0xFFFFF800
-                    d = (key & 0x0000003F)
-
-                    if base_key in self.serial_tx:
-                        node = self.serial_tx[base_key]
-
-                        vector = buffer.get(base_key, None)
-                        if vector is None:
-                            vector = [None]*node.size_in
-                            buffer[base_key] = vector
-                        vector[d] = value
-                        if None not in vector:
-                            node.output(now - start, vector)
-                            del buffer[base_key]
-                line = ''
-                retina = False
-
+        self.time_passed += self.dt
+        if self.time is None or self.time_passed < self.time:
+            self.timer = threading.Timer(self.dt, self.tick)
+            self.timer.start()
