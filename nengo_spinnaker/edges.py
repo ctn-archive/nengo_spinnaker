da4bffc3bcc0a391b18af2af042cb6ffbb55e3eb
AndrewMundy <andrew.mundy@ieee.org>
2014-05-02 10:35:44 +0100

    Factor `width` property out to provide `get_connection_width` in `edges.py`

--- nengo_spinnaker/edges.py
+++ nengo_spinnaker/edges.py
@@ -2,6 +2,16 @@ from pacman103.lib import graph
 import nengo
 
 
+def get_connection_width(connection):
+    """Return the width of a Connection."""
+    if isinstance(connection, int):
+        return connection
+    elif isinstance(connection.post, nengo.Ensemble):
+        return connection.post.dimensions
+    elif isinstance(connection.post, nengo.Node):
+        return connection.post.size_in
+
+
 class NengoEdge(graph.Edge):
     def __init__(self, conn, pre, post, constraints=None, label=None,
                  filter_is_accumulatory=True):
@@ -14,12 +24,7 @@ class NengoEdge(graph.Edge):
 
     @property
     def width(self):
-        if isinstance(self.conn, int):
-            return self.conn
-        elif isinstance(self.conn.post, nengo.Ensemble):
-            return self.conn.post.dimensions
-        elif isinstance(self.conn.post, nengo.Node):
-            return self.conn.post.size_in
+        get_connection_width(self.conn)
 
     def __getattr__(self, name):
         """Redirect missed attributes to the connection."""
