f4112dff1b3469b0fe3c7e5165fc6b8b89eb21f1
AndrewMundy <andrew.mundy@ieee.org>
2014-05-15 14:31:07 +0100

    Move FilterVertex to new layout.

--- nengo_spinnaker/filter_vertex.py
+++ nengo_spinnaker/filter_vertex.py
@@ -1,20 +1,11 @@
-from pacman103.lib import graph, data_spec_gen, lib_map
-from pacman103.front.common import enums
-
 from .utils import bins, filters, vertices
 
 
-class FilterVertex(graph.Vertex, filters.VertexWithFilters):
+@filters.with_filters(3, 4)
+class FilterVertex(vertices.NengoVertex):
     """PACMAN Vertex for a filtered external output node."""
-
-    REGIONS = enums.enum1(
-        'SYSTEM',
-        'OUTPUT_KEYS',
-        'FILTERS',
-        'FILTER_ROUTING',
-    )
-
-    model_name = "nengo_filter"
+    REGIONS = vertices.ordered_regions('SYSTEM', 'OUTPUT_KEYS')
+    MODEL_NAME = "nengo_filter"
 
     def __init__(self, dimensions, output_id, dt=0.001, time_step=1000,
                  output_period=100, constraints=None, label='filter'):
@@ -39,147 +30,51 @@ class FilterVertex(graph.Vertex, filters.VertexWithFilters):
                                            constraints=constraints,
                                            label=label)
 
-    def sizeof_region_system(self):
-        """Get the size (in bytes) of the SYSTEM region."""
-        # 5 words, 4 bytes per word
-        return 5 * 4
+    @vertices.region_pre_sizeof('SYSTEM')
+    def sizeof_region_system(self, n_atoms):
+        """Get the size (in words) of the SYSTEM region."""
+        return 5
 
-    def sizeof_region_output_keys(self):
-        """Get the size (in bytes) of the OUTPUT_KEYS region."""
-        # 1 word per output dimension
-        return 4 * self.dimensions
-
-    def sdram_usage(self, lo_atom, hi_atom):
-        """Return the amount of SDRAM used for the specified atoms."""
-        # At the moment this is the same as the DTCM usage, though this may
-        # change.
-        return self.dtcm_usage(lo_atom, hi_atom)
-
-    def dtcm_usage(self, lo_atom, hi_atom):
-        """Return the amount of DTCM used for the specified atoms."""
-        return sum([
-            self.sizeof_region_system(),
-            self.sizeof_region_output_keys(),
-            self.sizeof_region_filters(),
-            5 * 3 * 4 * len(self.filters)  # Assume that we will have at most 5
-                                           # subvertices feeding into a given
-                                           # filter. TODO Improve when possible
-        ])
+    @vertices.region_pre_sizeof('OUTPUT_KEYS')
+    def sizeof_region_output_keys(self, n_atoms):
+        """Get the size (in words) of the OUTPUT_KEYS region."""
+        return self.dimensions
 
     def cpu_usage(self, lo_atom, hi_atom):
         """Return the CPU utilisation for the specified atoms."""
         # TODO: Calculate this
         return 0
 
-    def get_resources_for_atoms(self, lo_atom, hi_atom, n_machine_time_steps,
-                                machine_time_step_us, partition_data_object):
-        """Get the resources required for the specified atoms.
-
-        :param lo_atom: Index of the lowest atom to represent
-        :param hi_atom: Index of the highest atom to represent
-        :param n_machine_time_steps: Duration of the simulation
-        :param machine_time_step_us: Duration of a machine time step in us
-        :param partition_data_object: ?
-
-        :returns: A tuple of the partition data object, and the resources
-                  required.
-        """
-        return lib_map.Resources(
-            self.cpu_usage(lo_atom, hi_atom),
-            self.dtcm_usage(lo_atom, hi_atom),
-            self.sdram_usage(lo_atom, hi_atom)
-        )
-
     def get_maximum_atoms_per_core(self):
         return 1
 
-    def generateDataSpec(self, processor, subvertex, dao):
-        """Generate the data spec for the given subvertex."""
-        # Create a spec for the subvertex
-        subvertex.spec = data_spec_gen.DataSpec(processor, dao)
-        subvertex.spec.initialise(0xABCE, dao)
-        subvertex.spec.comment("# Nengo Output Filter")
-
-        subvertex.output_keys = list()
-        x, y, p = processor.get_coordinates()
-
-        for d in range(self.dimensions):
-            subvertex.output_keys.append((x << 24) | (y << 16) |
-                                         ((p-1) << 11) |
-                                         (self.output_id << 6) | d)
-
-        # Fill in the spec
-        self.reserve_regions(subvertex)
-        self.write_region_system(subvertex)
-        self.write_region_output_keys(subvertex)
-
-        if len(self.filters) > 0:
-            self.write_region_filters(subvertex)
-            self.write_region_filter_keys(subvertex)
-
-        # Close the spec
-        subvertex.spec.endSpec()
-        subvertex.spec.closeSpecFile()
-
-        # Get the executable
-        x, y, p = processor.get_coordinates()
-        executable_target = lib_map.ExecutableTarget(
-            vertices.resource_filename("nengo_spinnaker",
-                                       "binaries/%s.aplx" % self.model_name),
-            x, y, p
-        )
-
-        return (executable_target, list(), list())
-
-    def reserve_regions(self, subvertex):
-        """Reserve sufficient space for the regions in the spec."""
-        # TODO Modify the following functions to use write_array rather than
-        #  lots of writes.
-        subvertex.spec.reserveMemRegion(
-            self.REGIONS.SYSTEM,
-            self.sizeof_region_system()
-        )
-        subvertex.spec.reserveMemRegion(
-            self.REGIONS.OUTPUT_KEYS,
-            self.sizeof_region_output_keys()
-        )
-        if len(self.filters) > 0:
-            subvertex.spec.reserveMemRegion(
-                self.REGIONS.FILTERS,
-                self.sizeof_region_filters()
-            )
-            subvertex.spec.reserveMemRegion(
-                self.REGIONS.FILTER_ROUTING,
-                self.sizeof_region_filter_keys(subvertex)
-            )
-
-    def write_region_system(self, subvertex):
+    @vertices.region_write('SYSTEM')
+    def write_region_system(self, subvertex, spec):
         """Write the system region for the given subvertex."""
-        subvertex.spec.switchWriteFocus(self.REGIONS.SYSTEM)
-        subvertex.spec.comment("""# System Region
-        # -------------
         # 1. Number of dimensions
         # 2. Machine time step in us
         # 3. Output period in ticks
         # 4. Number of filters
         # 5. Number of filter keys
-        """)
-        subvertex.spec.write(data=self.dimensions)
-        subvertex.spec.write(data=self.time_step)
-        subvertex.spec.write(data=self.output_period)
+        spec.write(data=self.dimensions)
+        spec.write(data=self.time_step)
+        spec.write(data=self.output_period)
 
         if len(self.in_edges) > 0:
-            subvertex.spec.write(data=len(self.filters))
-            subvertex.spec.write(data=self.filters.num_keys(subvertex))
+            spec.write(data=len(self.filters))
+            spec.write(data=self.filters.num_keys(subvertex))
         else:
-            subvertex.spec.write(data=0, repeats=2)
+            spec.write(data=0, repeats=2)
 
-    def write_region_output_keys(self, subvertex):
+    @vertices.region_write('OUTPUT_KEYS')
+    def write_region_output_keys(self, subvertex, spec):
         """Write the output keys region for the given subvertex."""
-        subvertex.spec.comment("# Output Keys Region")
-        subvertex.spec.switchWriteFocus(self.REGIONS.OUTPUT_KEYS)
-        for k in subvertex.output_keys:
-            subvertex.spec.write(data=k)
+        x, y, p = subvertex.placement.processor.get_coordinates()
+        i = self.output_id
+        key = (x << 24) | (y << 16) | ((p-1) << 11) | (i << 6)
+
+        for d in range(self.dimensions):
+            spec.write(data=key | d)
 
     def generate_routing_info(self, subedge):
         """Generate a key and mask for the given subedge."""
