ff08095d9aa7cd0dda90ae90f6d82cccb7d826a4
Andrew Mundy <andrew.mundy@ieee.org>
2014-06-02 09:22:03 +0100

    Neaten decoded value probe example.

--- examples/decoded_probe.py
+++ examples/decoded_probe.py
@@ -14,12 +14,11 @@ with model:
 # Simulate
 import nengo_spinnaker
 sim = nengo_spinnaker.Simulator(model)
-sim.run(10., clean=False)
+sim.run(10.)
 
 # Plot the output
 from matplotlib import pyplot as plt
 
 ts = np.arange(0, 10., step=0.001)
-plt.plot(ts, sim.data[p][0])
-plt.plot(ts, sim.data[p][1])
+plt.plot(ts, sim.data[p].T)
 plt.show()
