f76eb62d1277735963cbd4cf3b298ffe4610816f
james.knight <james.knight-5@postgrad.manchester.ac.uk>
2014-09-10 17:05:31 +0100

    More profiling script faffing - was profiling wrong ensembles and removing probes stopped output being encoded.

--- examples/profile_communication_channel.py
+++ examples/profile_communication_channel.py
@@ -1,82 +1,75 @@
 import collections
+import csv
 import sys
 import numpy as np
 import nengo
 import nengo_spinnaker
 import nengo_spinnaker.utils.profiling
+from nengo.utils.functions import whitenoise
 
-spinnaker = True
 dimensions = int(sys.argv[1]) if __name__ == "__main__" and len(sys.argv) > 1 else 1
 ensemble_size = int(sys.argv[2]) if __name__ == "__main__" and len(sys.argv) > 2 else 50
-plot = False
 
-from nengo.utils.functions import whitenoise
+num_learning_rules = int(sys.argv[3]) if __name__ == "__main__" and len(sys.argv) > 3 else 0
+num_post_populations = max(1, num_learning_rules)
+learning_rates = [1.0 / float(i + 1) for i in range(num_learning_rules)]
+
+profile_file = sys.argv[4] if __name__ == "__main__" and len(sys.argv) > 4 else "profile.csv"
+
+# **HACK** this is only correct for ensemble
+num_tags = 4
+
+print "Dimensions %u, Ensemble size %u, Learning rules %u (post populations %u)" % (dimensions, ensemble_size, num_learning_rules, num_post_populations)
+
 model = nengo.Network()
 with model:
     config = nengo_spinnaker.Config()
         
     inp = nengo.Node(whitenoise(0.1, 5, dimensions=dimensions), label = "inp")
+    inp_p = nengo.Probe(inp)
     config[inp].f_of_t = True
     
     pre = nengo.Ensemble(ensemble_size, dimensions=dimensions, label = "pre")
-    nengo.Connection(inp, pre)
-    post = nengo.Ensemble(ensemble_size, dimensions=dimensions, label = "post")
-    conn = nengo.Connection(pre, post, function=lambda x: np.random.random(dimensions))
-    config[post].profiler_num_samples = 10000
-    assert config[post].profiler_num_samples == 10000
-    
-    #if not spinnaker:
-    inp_p = nengo.Probe(inp)
-        
     pre_p = nengo.Probe(pre, synapse=0.01)
-    post_p = nengo.Probe(post, synapse=0.01)
+    nengo.Connection(inp, pre)
     
-    import matplotlib.pyplot as plt
-    if spinnaker:
-        sim = nengo_spinnaker.Simulator(model, config = config)
-        sim.run(0.1, clean = True)
-       
-        # **HACK** this is only correct for ensemble
-        tag_names = collections.defaultdict(lambda: "Unknown", 
-            { 0: "Timer", 1: "Input filter", 2: "Neuron update", 3: "Output" })
-        
-        print "Dimensions %u, Ensemble size %u" % (dimensions, ensemble_size)
-        for vertex, subvertices in sim.profile_data[post].iteritems():
+    posts = [nengo.Ensemble(ensemble_size, dimensions=dimensions, label = "post%u" % i) for i in range(num_post_populations)]
+    posts_p = [nengo.Probe(p, synapse = 0.01) for p in posts]
+    conns = [nengo.Connection(pre, p, function=lambda x: np.random.random(dimensions)) for p in posts]
+    
+    # Profile pre ensemble
+    config[pre].profiler_num_samples = 10000
+    
+    if len(learning_rates) > 0:
+        errors = [nengo.Ensemble(ensemble_size, dimensions=dimensions, label = "error%u" % i) for i, l in enumerate(learning_rates)]
+        for e, p, c, l in zip(errors, posts, conns, learning_rates):
+            # Error = pre - post
+            nengo.Connection(pre, e)
+            nengo.Connection(p, e, transform=-1)
+            
+            # Modulatory connections don't impart current
+            error_conn = nengo.Connection(e, p, modulatory=True)
+            
+            # Add the learning rule to the connection
+            c.learning_rule = nengo.PES(error_conn, learning_rate = l)
+
+    sim = nengo_spinnaker.Simulator(model, config = config)
+    sim.run(0.1, clean = True)
+    
+    with open(profile_file, "ab") as csvFile:
+        csv_writer = csv.writer(csvFile)
+        for vertex, subvertices in sim.profile_data[pre].iteritems():
             for subvertex, data in subvertices.iteritems():
+                # Get subvertex's placement
+                (x, y, p) = subvertex.placement.processor.get_coordinates()
+                
+                # Extract profiling data from this core
                 plot_data = nengo_spinnaker.utils.profiling.plot_profile_data(data)
                 
-                if plot:
-                    figure, axis = plt.subplots()
+                csv_row = [dimensions, ensemble_size, num_learning_rules, x, y, p]
+                csv_row.extend([None for i in range(num_tags)])
                 
                 for tag, times in plot_data.iteritems():
-                    if plot:
-                        for start_time, duration in zip(times[0], times[1]):
-                            axis.bar(tag, duration, bottom = start_time, width = 1.0, linewidth = None)
-                    
-                    print "%s min:%fms, max:%fms, mean:%fms" % (tag_names[tag], np.amin(times[1]), np.amax(times[1]), np.mean(times[1]))
+                    csv_row[tag + 6] = np.mean(times[1])
                 
-                if plot:
-                    axis.set_xticks([0.5 + float(i) for i in plot_data])
-                    axis.set_xticklabels([tag_names[i] for i in plot_data])
-                
-        
-    else:
-        sim = nengo.Simulator(model)
-        sim.run(1.0)
-    
-    if plot:
-        plt.figure(figsize=(12, 8))
-        
-        for d in range(dimensions):
-            #if dimensions > 1:
-            plt.subplot(dimensions, 1, d + 1)
-            
-            #if not spinnaker:
-            plt.plot(sim.trange(), sim.data[inp_p].T[d], c='k', label='Input')
-                
-            plt.plot(sim.trange(), sim.data[pre_p].T[d], c='b', label='Pre')
-            plt.plot(sim.trange(), sim.data[post_p].T[d], c='r', label='Post')
-            plt.ylabel("Dimension 1")
-            plt.legend(loc='best')
-    
-        plt.show()
+                csv_writer.writerow(csv_row)
--- examples/profile_communication_channel.sh
+++ examples/profile_communication_channel.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 dimensions=(1 2 5 10 20 50)
 ensemble_sizes=(10 20 50 100 200 500 1000 2000)
+num_learning_rules=(0 1 2 3 4)
 
 for d in "${dimensions[@]}"
 do
 for e in "${ensemble_sizes[@]}"
 do
-python profile_communication_channel.py $d $e
+for l in "${num_learning_rules[@]}"
+do
+python profile_communication_channel.py $d $e $l $1
+done
 done
 done
