934fb83978830d2d1cd68885c79346c8af12ea1c
Andrew Mundy <andrew.mundy@ieee.org>
2014-10-06 08:54:01 +0100

    Add vertex resource calculations.

--- nengo_spinnaker/spinnaker/tests/test_vertices.py
+++ nengo_spinnaker/spinnaker/tests/test_vertices.py
@@ -1,6 +1,67 @@
 from ..vertices import NengoVertex
+from .. import regions
 
 
 ## Region requirements tests
 def test_get_sdram_usage_for_atoms():
-    assert False, "Test not written yet."
+    """Test that the SDRAM usage for various regions is reported correctly.
+    """
+    rs = [
+        regions.MatrixRegion(shape=(100, 5), prepend_full_length=True,
+                             prepend_n_atoms=True),
+        regions.MatrixRegionPartitionedByColumns(shape=(5, 100),
+                                                 unfilled=True),
+        regions.MatrixRegionPartitionedByRows(shape=(100, 5))
+    ]
+    v = NengoVertex(100, '', rs)
+
+    assert (v.get_sdram_usage_for_atoms(slice(0, 10)) ==
+            4*sum(r.sizeof(slice(0, 10)) for r in rs))
+
+
+def test_get_dtcm_usage_for_regions():
+    """Test that the DTCM usage for various regions is reported correctly.
+    """
+    rs = [
+        regions.MatrixRegion(shape=(100, 5), prepend_full_length=True,
+                             prepend_n_atoms=True),
+        regions.MatrixRegionPartitionedByColumns(shape=(5, 100),
+                                                 unfilled=True),
+        regions.MatrixRegionPartitionedByRows(shape=(100, 5))
+    ]
+    v = NengoVertex(100, '', rs)
+
+    assert (v.get_dtcm_usage_for_atoms(slice(0, 10)) ==
+            4*sum(r.sizeof(slice(0, 10)) for r in rs))
+
+
+def test_get_dtcm_usage_for_regions_non_dtcm_regions():
+    """Test that the DTCM usage for various regions is reported correctly when
+    some of the regions are not stored in DTCM.
+    """
+    rs = [
+        regions.MatrixRegion(shape=(100, 5), prepend_full_length=True,
+                             prepend_n_atoms=True),
+        regions.MatrixRegionPartitionedByColumns(shape=(5, 100),
+                                                 unfilled=True),
+        regions.MatrixRegionPartitionedByRows(shape=(100, 5), in_dtcm=False)
+    ]
+    v = NengoVertex(100, '', rs)
+
+    assert (v.get_dtcm_usage_for_atoms(slice(0, 10)) ==
+            4*sum(r.sizeof(slice(0, 10)) for r in rs[:-1]))
+
+
+def test_get_dtcm_usage_with_other_costs():
+    rs = [
+        regions.MatrixRegion(shape=(100, 5), prepend_full_length=True,
+                             prepend_n_atoms=True),
+        regions.MatrixRegionPartitionedByColumns(shape=(5, 100),
+                                                 unfilled=True),
+        regions.MatrixRegionPartitionedByRows(shape=(100, 5), in_dtcm=False)
+    ]
+    v = NengoVertex(100, '', rs)
+    v.get_dtcm_usage_static = lambda sl: 10*(sl.stop - sl.start)
+
+    assert (v.get_dtcm_usage_for_atoms(slice(0, 10)) ==
+            4*sum(r.sizeof(slice(0, 10)) for r in rs[:-1]) + 4*100)
--- nengo_spinnaker/spinnaker/vertices.py
+++ nengo_spinnaker/spinnaker/vertices.py
@@ -1,7 +1,13 @@
+from pacman.model.resources.resource_container import ResourceContainer
+from pacman.model.resources.cpu_cycles_per_tick_resource import \
+    CPUCyclesPerTickResource
+from pacman.model.resources.dtcm_resource import DTCMResource
+from pacman.model.resources.sdram_resource import SDRAMResource
+
+
 class NengoVertex(object):
     """Helper for constructing Vertices for PACMAN."""
-    def __init__(self, n_atoms, label, executable, max_atoms_per_core,
-                 regions=list(), constraints=None):
+    def __init__(self, n_atoms, label, regions=list(), constraints=None):
         """Create a new NengoVertex object.
 
         Each NengoVertex object consists of a set of regions, each region
@@ -10,11 +16,48 @@ class NengoVertex(object):
         :param int n_atoms: Number of processing atoms represented by the
                             vertex.
         :param string label: Human readable representation for the vertex.
-        :param string executable: Complete path of executable to use for
-                                  subvertices of this vertex.
-        :param int max_atoms_per_core: The maximum number of atoms to allocate
-                                       to a processor core.
         :param list regions: A list of memory regions for the vertex.
         :param list constraints: A list of constraints for the vertex.
+        """
+        self.n_atoms = n_atoms
+        self.label = label
+        self.regions = regions
+        self.contraints = constraints
+        self._n_subvertices = 0
+
+    def get_resources_used_by_atoms(self, vertex_slice, graph):
+        return ResourceContainer(
+            cpu=CPUCyclesPerTickResource(
+                self.get_cpu_usage_for_atoms(vertex_slice)),
+            dtcm=DTCMResource(self.get_dtcm_usage_for_atoms(vertex_slice)),
+            sdram=SDRAMResource(self.get_sdram_usage_for_atoms(vertex_slice))
+        )
+
+    def get_sdram_usage_for_atoms(self, vertex_slice):
+        """Get the SDRAM usage for the given slice of the vertex.
+
+        This calculation returns the total memory (in BYTES) used in SDRAM.
+        """
+        return 4*sum(r.sizeof(vertex_slice) for r in self.regions)
+
+    def get_dtcm_usage_for_atoms(self, vertex_slice):
+        """Get the DTCM usage for the given slice of the vertex.
+
+        This calculation returns the total memory (in BYTES) used in DTCM.
+        """
+        words = sum(r.sizeof(vertex_slice) for r in self.regions if r.in_dtcm)
+        words += self.get_dtcm_usage_static(vertex_slice)
+
+        return 4 * words
+
+    def get_dtcm_usage_static(self, vertex_slice):
+        """Get the non-region related DTCM usage for the given number of atoms.
+
+        This calculation returns the total memory (in BYTES) used in DTCM.
+        """
+        return 0
+
+    def get_cpu_usage_for_atoms(self, vertex_slice):
+        """Get the CPU usage (in ticks per step) for the given vertex slice.
         """
         raise NotImplementedError
