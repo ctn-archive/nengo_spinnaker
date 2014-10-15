da051ac123aa13f467dafd15c178d970914e57cd
Andrew Mundy <andrew.mundy@ieee.org>
2014-10-15 11:14:18 +0100

    Connection replacing method.
    Adds method to replace connections whose objects have been replaced.

--- nengo_spinnaker/utils/connections.py
+++ nengo_spinnaker/utils/connections.py
@@ -2,6 +2,41 @@
 """
 
 import collections
+import copy
+import nengo
+
+from .. import connection
+
+
+def replace_objects_in_connections(connections, replaced_objects):
+    """Replace connections where the pre- or post-object has been replaced.
+
+    :param iterable connections: A list of connections to process.
+    :param dict replaced_objects: A mapping of old to new objects.
+    :returns iterable: A list of connections where connections that included
+        references to old objects have been replaced with connections with
+        references to new objects.
+    """
+    new_connections = []
+    for c in connections:
+        if (c.pre_obj not in replaced_objects and
+                c.post_obj not in replaced_objects):
+            new_connections.append(c)
+            continue
+
+        if isinstance(c, nengo.Connection):
+            new_c = connection.IntermediateConnection.from_connection(c)
+        else:
+            new_c = copy.copy(c)
+
+        if c.pre_obj in replaced_objects:
+            new_c.pre_obj = replaced_objects[c.pre_obj]
+        if c.post_obj in replaced_objects:
+            new_c.post_obj = replaced_objects[c.post_obj]
+
+        new_connections.append(new_c)
+
+    return new_connections
 
 
 class TransformFunctionKeyspaceConnection(
--- nengo_spinnaker/utils/tests/test_connections.py
+++ nengo_spinnaker/utils/tests/test_connections.py
@@ -1,8 +1,66 @@
 import mock
 import nengo
 import numpy as np
+import random
 
 from .. import connections as connections_utils
+
+
+def test_replace_object_in_connections():
+    """Test that connections are replaced with new connections when they
+    originate or terminate with objects which are to be replaced.
+    """
+    class FalseConnection(object):
+        def __init__(self, pre_obj, post_obj):
+            self.pre_obj = pre_obj
+            self.post_obj = post_obj
+
+        def __repr__(self):
+            return "FalseConnection({}, {})".format(self.pre_obj,
+                                                    self.post_obj)
+
+    class FalseEnsemble(object):
+        def __repr__(self):
+            return '<FalseEnsemble at {:#x}>'.format(id(self))
+
+    class FalseIntermediateEnsemble(object):
+        def __repr__(self):
+            return '<FalseIntermediateEnsemble at {:#x}>'.format(id(self))
+
+    # Create some non-replaced objects and some replaced objects
+    non_replaced_obj = [FalseEnsemble() for n in range(2)]
+    replaced_objs = {FalseEnsemble(): FalseIntermediateEnsemble() for n in
+                     range(3)}
+
+    # Create a complete set of connections between these sets of objects,
+    # randomly remove some elements.
+    connections = list()
+    connections.extend(FalseConnection(nro, ro) for nro in non_replaced_obj for
+                       ro in replaced_objs.keys())
+    connections.extend(FalseConnection(ro, nro) for nro in non_replaced_obj for
+                       ro in replaced_objs.keys())
+    random.shuffle(connections)
+    connections = connections[:-3]
+
+    # Add one connection which will remain the same
+    connections.append(
+        FalseConnection(non_replaced_obj[0], non_replaced_obj[1]))
+
+    # Replace connections where objects have also been replaced
+    new_connections = connections_utils.\
+        replace_objects_in_connections(connections, replaced_objs)
+
+    # Check for success
+    assert len(connections) == len(new_connections)
+    assert connections[-1] in new_connections
+
+    for (oc, nc) in zip(connections[:-1], new_connections):
+        assert nc is not oc
+
+        if oc.pre_obj in replaced_objs:
+            assert nc.pre_obj is replaced_objs[oc.pre_obj]
+        if oc.post_obj in replaced_objs:
+            assert nc.post_obj is replaced_objs[oc.post_obj]
 
 
 def test_TransformFunctionKeyspaceConnection():
