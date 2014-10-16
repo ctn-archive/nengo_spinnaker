10fc463a3cdbeaebc8690051b9fa945129754612
AndrewMundy <andrew.mundy@ieee.org>
2014-10-16 14:36:30 +0100

    Rewrite of filter region functionality.

--- nengo_spinnaker/utils/filters.py
+++ nengo_spinnaker/utils/filters.py
@@ -3,64 +3,89 @@
 
 import collections
 import nengo
+import numpy as np
+
+from . import fixpoint as fp
+from ..spinnaker import regions
 
 
-class SynapseParameter(object):
-    def __init__(self, is_accumulatory=False, is_modulatory=False):
+class FilterParameter(object):
+    """Base class for filter types."""
+    def __init__(self, width, is_accumulatory=True, is_modulatory=False):
+        # TODO: Is width actually required?  Keep it to maintain compatibility
+        #       current C code, but investigate removing it if we can.
+        self.width = width
         self.is_accumulatory = is_accumulatory
         self.is_modulatory = is_modulatory
+
+    @classmethod
+    def from_synapse(cls, synapse, is_accumulatory=True, is_modulatory=False):
+        raise NotImplementedError
 
     def __eq__(self, other):
         return (self.__class__ is other.__class__ and
                 self.is_accumulatory == other.is_accumulatory and
-                self.is_modulatory == other.is_modulatory)
+                self.is_modulatory == other.is_modulatory and
+                self.width == other.width)
 
     def __hash__(self):
         return hash((hash(self.__class__), hash(self.is_accumulatory),
-                     hash(self.is_modulatory)))
+                     hash(self.is_modulatory), hash(self.width)))
 
 
-class LinearFilterSynapseParameter(SynapseParameter):
-    def __init__(self, synapse, is_accumulatory=False, is_modulatory=False):
-        super(LinearFilterSynapseParameter, self).__init__(is_accumulatory,
-                                                           is_modulatory)
-        self.num = synapse.num
-        self.den = synapse.den
+class LinearFilterFilterParameter(FilterParameter):
+    def __init__(self, width, num, den, is_accumulatory=True,
+                 is_modulatory=False):
+        super(LinearFilterFilterParameter, self).__init__(
+            width, is_accumulatory, is_modulatory)
+        self.num = num
+        self.den = den
+
+    @classmethod
+    def from_synapse(cls, width, synapse, is_accumulatory=True,
+                     is_modulatory=False):
+        return cls(width, synapse.num, synapse.den, is_accumulatory,
+                   is_modulatory)
 
     def __eq__(self, other):
-        return (super(LinearFilterSynapseParameter, self).__eq__(other) and
+        return (super(LinearFilterFilterParameter, self).__eq__(other) and
                 self.num == other.num and
                 self.den == other.den)
 
     def __hash__(self):
-        return hash((super(LinearFilterSynapseParameter, self).__hash__(),
+        return hash((super(LinearFilterFilterParameter, self).__hash__(),
                      hash(self.num), hash(self.den)))
 
 
-class LowpassSynapseParameter(SynapseParameter):
-    def __init__(self, synapse, is_accumulatory=False, is_modulatory=False):
-        super(LowpassSynapseParameter, self).__init__(is_accumulatory,
-                                                      is_modulatory)
-        self.tau = synapse.tau
+class LowpassFilterParameter(FilterParameter):
+    def __init__(self, width, tau, is_accumulatory=True, is_modulatory=False):
+        super(LowpassFilterParameter, self).__init__(width, is_accumulatory,
+                                                     is_modulatory)
+        self.tau = tau if tau is not None else 0.
+
+    @classmethod
+    def from_synapse(cls, width, synapse, is_accumulatory=True,
+                     is_modulatory=False):
+        return cls(width, synapse.tau, is_accumulatory, is_modulatory)
 
     def __eq__(self, other):
-        return (super(LowpassSynapseParameter, self).__eq__(other) and
+        return (super(LowpassFilterParameter, self).__eq__(other) and
                 self.tau == other.tau)
 
     def __hash__(self):
-        return hash((super(LowpassSynapseParameter, self).__hash__(),
+        return hash((super(LowpassFilterParameter, self).__hash__(),
                      hash(self.tau)))
 
 
-class AlphaSynapseParameter(LowpassSynapseParameter):
+class AlphaFilterParameter(LowpassFilterParameter):
     # Obviously this is a different type, but it has the same parameters as the
     # lowpass and so can derive from it safely (DRY).
     pass
 
 
-_FilterTypes = {nengo.synapses.LinearFilter: LinearFilterSynapseParameter,
-                nengo.Lowpass: LowpassSynapseParameter,
-                nengo.synapses.Alpha: AlphaSynapseParameter, }
+_FilterTypes = {nengo.synapses.LinearFilter: LinearFilterFilterParameter,
+                nengo.Lowpass: LowpassFilterParameter,
+                nengo.synapses.Alpha: AlphaFilterParameter, }
 
 
 def get_combined_filters(connections):
@@ -71,8 +96,12 @@ def get_combined_filters(connections):
     # Create a dictionary of filter to connections
     filter_connections = collections.defaultdict(list)
     for c in connections:
-        f = _FilterTypes[c.synapse.__class__](
-            c.synapse, getattr(c, 'is_accumulatory', False), c.modulatory)
+        f = _FilterTypes[c.synapse.__class__].from_synapse(
+            c.width,
+            c.synapse,
+            getattr(c, 'is_accumulatory', True),
+            c.modulatory
+        )
         filter_connections[f].append(c)
 
     # Create a list of filters and a mapping of connection to filter index.
@@ -86,3 +115,74 @@ def get_combined_filters(connections):
             filter_indices[c] = filters.index(f)
 
     return filters, filter_indices
+
+
+def get_keyspace_to_filter_map(filter_indices):
+    """Convert a mapping from connection to filter index to keyspace to indices
+    """
+    return {c.keyspace: v for (c, v) in filter_indices.items()}
+
+
+def get_filter_regions(connections, dt):
+    """Return the filter and routing region for the given connections.
+
+    Returns a tuple of (filter region, routing region).
+    """
+    # Get the minimum set of filters
+    filters, filter_ids = get_combined_filters(connections)
+
+    # Create and return the appropriate regions
+    return make_filter_region(filters, dt), make_routing_region(filter_ids)
+
+
+def make_filter_region(filters, dt):
+    """Return a region representing parameters for incoming value filters.
+
+    TODO: Support more than just 1st order low-pass filters.
+    """
+    for f in filters:
+        assert isinstance(f, LowpassFilterParameter)
+
+    # Build up the filter coefficients
+    cs = np.exp(-dt / np.array([f.tau for f in filters]))
+    ics = 1. - cs
+
+    fpc = np.array(fp.bitsk(cs), dtype=np.uint32)
+    fpd = np.array(fp.bitsk(ics), dtype=np.uint32)
+
+    # Get the accumulator masks for the filters and the widths of the filters
+    masks = np.array([0x0 if f.is_accumulatory else 0xffffffff for f in
+                      filters], dtype=np.uint32)
+    widths = np.array([f.width for f in filters], dtype=np.uint32)
+
+    # Stack these columns to make the full filter matrix
+    f_matrix = np.vstack([fpc, fpd, masks, widths]).T
+    assert f_matrix.shape == (len(filters), 4)
+
+    # Once we've computed the data to store, this region is just an
+    # unpartitioned matrix region which prepends the number of rows
+    # to the block of data when writing out.
+    return regions.MatrixRegion(
+        matrix=f_matrix,
+        prepends=[regions.MatrixRegionPrepends.N_ROWS]
+    )
+
+
+def make_routing_region(filter_ids):
+    """Return a region representing filter routing information.
+
+    The data written out is an array, each entry contains:
+     - The filter routing key to match
+     - The filter mask to apply to incoming packets
+     - The index of the filter packets matching this entry should use
+     - The mask to apply to get the component index from the key
+    """
+    filter_ids = get_keyspace_to_filter_map(filter_ids)
+    return regions.KeysRegion(
+        filter_ids.keys(),
+        extra_fields=[
+            lambda ks, i: ks.filter_mask,  # Filter mask
+            lambda ks, i: filter_ids[ks],  # Filter index
+            lambda ks, i: ks.mask_d,       # Dimension mask
+        ],
+        prepend_n_keys=True)
--- nengo_spinnaker/utils/tests/test_filters.py
+++ nengo_spinnaker/utils/tests/test_filters.py
@@ -1,11 +1,15 @@
+import mock
 import nengo
+import numpy as np
+import pytest
 
+from ... import connection
 from .. import filters as filter_utils
+from ..fixpoint import bitsk
 
 
-def test_get_combined_filters():
-    """Test that filters may be combined correctly.
-    """
+@pytest.fixture(scope='function')
+def sample_network():
     model = nengo.Network()
     with model:
         a = nengo.Ensemble(100, 1)
@@ -18,7 +22,7 @@ def test_get_combined_filters():
         # 4 + 5 share a filter
         # 6 is a unique filter (because it is accumulatory)
         # 7 is a unique filter (because it is modulatory)
-        conns = [
+        cs = [
             nengo.Connection(a, b, synapse=0.01),
             nengo.Connection(a, b, transform=0.1, synapse=0.01),
             nengo.Connection(a, b, synapse=0.05),
@@ -29,7 +33,17 @@ def test_get_combined_filters():
             nengo.Connection(a, b, synapse=0.05, modulatory=True),
         ]
 
-        setattr(conns[6], 'is_accumulatory', True)
+        conns = [connection.IntermediateConnection.from_connection(c) for c in
+                 cs]
+        conns[6].is_accumulatory = False
+
+    return model, conns
+
+
+def test_get_combined_filters(sample_network):
+    """Test that filters may be combined correctly.
+    """
+    (_, conns) = sample_network
 
     # Construct the minimum set of filters and provide indices from connections
     # into the set of filters.
@@ -50,7 +64,118 @@ def test_get_combined_filters():
     # Assert that parameters have been grabbed correctly
     for i in [0, 2, 3, 4, 7]:
         assert filters[filter_indices[conns[i]]].tau == conns[i].synapse.tau
-        assert not filters[filter_indices[conns[i]]].is_accumulatory
+        assert filters[filter_indices[conns[i]]].is_accumulatory
 
     assert filters[filter_indices[conns[6]]].tau == conns[6].synapse.tau
-    assert filters[filter_indices[conns[6]]].is_accumulatory
+    assert not filters[filter_indices[conns[6]]].is_accumulatory
+
+
+def test_get_keyspace_to_filter_map(sample_network):
+    """Test that a map from keyspace to filter index can be made.
+    """
+    (_, conns) = sample_network
+
+    # Convert each connection to an intermediate connection with a unique
+    # "keyspace".
+    for c in conns:
+        c.keyspace = mock.Mock()
+
+    # Construct the minimum set of filters and provide indices from connections
+    # into the set of filters.
+    filters, filter_indices = filter_utils.get_combined_filters(conns)
+
+    # Now get the map of keyspace to filter index
+    filter_indices_ks = filter_utils.get_keyspace_to_filter_map(filter_indices)
+
+    # Assert this is correct by comparing across dictionaries
+    for c in conns:
+        assert filter_indices[c] == filter_indices_ks[c.keyspace]
+
+
+def test_get_filter_regions(sample_network):
+    """Test that an appropriate filter and keys region are produced.
+    """
+    (_, conns) = sample_network
+
+    # Convert each connection to an intermediate connection with a unique
+    # "keyspace".
+    for c in conns:
+        c.keyspace = mock.Mock()
+
+    # Assign a key and mask to each keyspace
+    for i, c in enumerate(conns):
+        c.keyspace.key.return_value = i
+        c.keyspace.filter_mask.return_value = 0xffffffff
+
+    # (For testing purposes)
+    filters, filter_ids = filter_utils.get_combined_filters(conns)
+    filter_ids_ks = filter_utils.get_keyspace_to_filter_map(filter_ids)
+
+    # Get the filter regions
+    dt = 0.001
+    filter_region, filter_routing_region = \
+        filter_utils.get_filter_regions(conns, dt)
+
+    # Assert the size of these regions is appropriate
+    # Filter region should be 4 * n_filters (4 words per filter) + 1 (n filters)
+    assert filter_region.sizeof(slice(0, 100)) == len(filters) * 4 + 1
+
+    # Keys region should be 4 * n_connections (4 words per connection) + 1
+    assert filter_routing_region.sizeof(slice(0, 100)) == len(conns) * 4 + 1
+
+
+def test_filter_region():
+    """Test that the region writes out filter parameters correctly.
+    """
+    # Create 2 filter entries
+    filters = [
+        filter_utils.LowpassFilterParameter(1, 0.05, True, False),
+        filter_utils.LowpassFilterParameter(2, 0.01, False, False),
+    ]
+
+    # Create the filter region
+    dt = 0.01
+    filter_region = filter_utils.make_filter_region(filters, dt)
+
+    # Check the size is correct
+    assert filter_region.sizeof(slice(0, 1000)) == 2*4 + 1
+
+    # Check the data is correct
+    sr = filter_region.create_subregion(slice(25, 50), 0)
+    data = np.frombuffer(sr.data, dtype=np.uint32)
+
+    assert len(data) == 2*4 + 1
+    assert data[0] == 2  # Number of filters
+    for i, f in enumerate(filters):
+        assert data[1 + 4*i] == bitsk(np.exp(-dt / f.tau))
+        assert data[2 + 4*i] == bitsk(1. - np.exp(-dt / f.tau))
+        assert data[3 + 4*i] == 0x0 if f.is_accumulatory else 0xffffffff
+        assert data[4 + 4*i] == f.width
+
+
+def test_filter_routing_region():
+    """Test that the region writes out filter routing values correctly.
+    """
+    # Create a mock keyspace to check that values are collected correctly.
+    keyspace = mock.Mock(spec_set=['key', 'filter_mask', 'mask_d'])
+    keyspace.key.return_value = 0xCAFECAFE
+    keyspace.filter_mask = 0xBEEFBEEF
+    keyspace.mask_d = 0xDEADABCD
+
+    connection = mock.Mock(spec_set=['keyspace'])
+    connection.keyspace = keyspace
+
+    # Create a mapping from keyspace to filter ID
+    filter_ids = {connection: 0xFEFEFFFF}
+
+    # Create the region
+    routing_region = filter_utils.make_routing_region(filter_ids)
+    assert routing_region.sizeof(slice(0, 100)) == 4+1
+
+    # Create a subregion version and ensure that the data stored within is
+    # correct.
+    sr = routing_region.create_subregion(slice(0, 100), 0)
+    data = np.frombuffer(sr.data, dtype=np.uint32)
+    assert data[0] == 1
+    assert data[1:].tolist() == [keyspace.key(), keyspace.filter_mask,
+                                 filter_ids[connection], keyspace.mask_d]
