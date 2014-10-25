1ed9e6eb0e6476a4c83527b99cf28d1395b15ffe
AndrewMundy <andrew.mundy@ieee.org>
2014-10-25 17:16:39 +0100

    Complete connection tree data types.
    Neatens up parts of code, adds receiving end tree elements.

--- nengo_spinnaker/utils/connection_tree.py
+++ nengo_spinnaker/utils/connection_tree.py
@@ -11,7 +11,13 @@ tree.  Only certain changes are allowed to alter the structure of the tree.
 import numpy as np
 
 
-class ReducedConnection(object):
+class OutgoingReducedConnection(object):
+    """Represents the limited information required to transmit data.
+
+    The minimum set of parameters to transmit information are the transform
+    provided on a connection, the function computed on the connection and the
+    keyspace (if any) attached to the connection.
+    """
     __slots__ = ['transform', 'function', 'keyspace']
 
     # Comparisons between connections: ReducedConnections are equivalent iff.
@@ -47,24 +53,29 @@ class ReducedConnection(object):
                      self.keyspace))
 
     def __eq__(self, other):
-        # Equality is managed by checking the result of a generator, _eqs
-        return all(self.__eqs(other))
-
-    def __eqs(self, other):
-        for fn in self._eq_terms:
-            yield fn(self, other)
+        return all(fn(self, other) for fn in self._eq_terms)
 
 
-class ReducedEnsembleConnection(ReducedConnection):
+class OutgoingReducedEnsembleConnection(OutgoingReducedConnection):
+    """Represents the limited information required to transmit ensemble data.
+
+    The minimum set of parameters to transmit information are the transform
+    provided on a connection, the function computed on the connection, the
+    keyspace (if any) attached to the connection; for ensembles some additional
+    components are necessary: the evaluation points for decoder solving, the
+    specific solver and any learning rules which modify the transmitted value.
+    """
     __slots__ = ['eval_points', 'solver', 'transmitter_learning_rules']
 
     # ReducedEnsembleConnections are equivalent iff. they meet they share a
-    # class, a keyspace, a solver, a transform, eval points and a function
-    # (evaluated on those eval points).
+    # class, a keyspace, a solver, a transform, eval points, a function
+    # (evaluated on those eval points) and have NO learning rules.
     _eq_terms = [
         lambda a, b: a.__class__ is b.__class__,
         lambda a, b: a.keyspace == b.keyspace,
         lambda a, b: a.solver == b.solver,
+        lambda a, b: len(a.transmitter_learning_rules) == 0,
+        lambda a, b: len(b.transmitter_learning_rules) == 0,
         lambda a, b: np.all(a.transform == b.transform),
         lambda a, b: np.all(a.eval_points == b.eval_points),
         lambda a, b: np.all(a._get_evaluated_function() ==
@@ -72,17 +83,19 @@ class ReducedEnsembleConnection(ReducedConnection):
     ]
 
     def __init__(self, transform, function, keyspace=None, eval_points=None,
-                 solver=None):
-        super(ReducedEnsembleConnection, self).__init__(transform, function,
-                                                        keyspace)
+                 solver=None, transmitter_learning_rules=list()):
+        super(OutgoingReducedEnsembleConnection, self).__init__(
+            transform, function, keyspace)
         self.eval_points = np.array(eval_points).copy()
         self.eval_points.flags.writeable = False
         self.solver = solver
+        self.transmitter_learning_rules = tuple(transmitter_learning_rules)
 
     def __hash__(self):
         return hash((self.__class__, self.transform.data, self.keyspace,
                      self.solver, self.eval_points.data,
-                     self._get_evaluated_function().data))
+                     self._get_evaluated_function().data,
+                     self.transmitter_learning_rules))
 
     def _get_evaluated_function(self):
         """Evaluate the function at eval points and return Numpy array.
@@ -91,3 +104,55 @@ class ReducedEnsembleConnection(ReducedConnection):
                 else self.eval_points)
         data.flags.writeable = False
         return data
+
+
+class IncomingReducedConnection(object):
+    """Represents the limited information required to receive data.
+
+    The minimum set of parameters to transmit information are the object that
+    is receiving the data and the filter used.
+    """
+    __slots__ = ['target', 'filter_object']
+
+    # Incoming reduced connections are equivalent iff. they share a receiving
+    # object (target) and have equivalent connections.
+    _eq_terms = [
+        lambda a, b: a.__class__ is b.__class__,
+        lambda a, b: a.target is b.target,
+        lambda a, b: a.filter_object == b.filter_object,
+    ]
+
+    def __init__(self, target, filter_object):
+        self.target = target
+        self.filter_object = filter_object
+
+    def __eq__(self, other):
+        return all(fn(self, other) for fn in self._eq_terms)
+
+    def __hash__(self):
+        return hash((self.target, self.filter_object))
+
+
+class IncomingReducedEnsembleConnection(IncomingReducedConnection):
+    """Represents the limited information required to receive data.
+
+    The minimum set of parameters to transmit information are the object that
+    is receiving the data and the filter used.
+    """
+    __slots__ = ['receiver_learning_rules']
+
+    # Incoming reduced connections are equivalent iff. they share a receiving
+    # object (target), have equivalent connections and have NO learning rules.
+    _eq_terms = IncomingReducedConnection._eq_terms + [
+        lambda a, b: len(a.receiver_learning_rules) == 0,
+        lambda a, b: len(b.receiver_learning_rules) == 0,
+    ]
+
+    def __init__(self, target, filter_object, receiver_learning_rules=list()):
+        super(IncomingReducedEnsembleConnection, self).__init__(
+            target, filter_object)
+        self.receiver_learning_rules = tuple(receiver_learning_rules)
+
+    def __hash__(self):
+        return hash((super(IncomingReducedEnsembleConnection, self).__hash__(),
+                     self.receiver_learning_rules))
--- nengo_spinnaker/utils/tests/test_connection_tree.py
+++ nengo_spinnaker/utils/tests/test_connection_tree.py
@@ -3,19 +3,22 @@ import mock
 import numpy as np
 
 from .. import connection_tree
-from ..connection_tree import ReducedConnection, ReducedEnsembleConnection
+from ..connection_tree import (
+    OutgoingReducedConnection, OutgoingReducedEnsembleConnection,
+    IncomingReducedConnection, IncomingReducedEnsembleConnection,
+)
 
 
-class TestReducedConnection(object):
+class TestOutgoingReducedConnection(object):
     def test_eq(self):
         # Create some reduced connections and test for equivalence
         rcs = [
-            ReducedConnection(np.eye(3), None),
-            ReducedConnection(np.eye(3), None),
-            ReducedConnection(np.eye(3), lambda x: x**2),
-            ReducedConnection(np.eye(1), None),
-            ReducedConnection(np.eye(3), None, mock.Mock()),
-            ReducedEnsembleConnection(np.eye(3), None),
+            OutgoingReducedConnection(np.eye(3), None),
+            OutgoingReducedConnection(np.eye(3), None),
+            OutgoingReducedConnection(np.eye(3), lambda x: x**2),
+            OutgoingReducedConnection(np.eye(1), None),
+            OutgoingReducedConnection(np.eye(3), None, mock.Mock()),
+            OutgoingReducedEnsembleConnection(np.eye(3), None),
         ]
 
         # Define what the equivalences should be
@@ -33,8 +36,8 @@ class TestReducedConnection(object):
     def test_copy(self):
         # Ensure that when we copy we get copies of the transform but
         # references to everything else.
-        c = connection_tree.ReducedConnection(np.eye(3), lambda x: x**2,
-                                              mock.Mock())
+        c = connection_tree.OutgoingReducedConnection(
+            np.eye(3), lambda x: x**2, mock.Mock())
         d = copy.copy(c)
 
         # Assert the transforms are equivalent but not the same object
@@ -49,7 +52,7 @@ class TestReducedConnection(object):
     def test_copy_with_transform(self):
         # Ensure that we can copy a reduced connection but modify the transform
         # in the process.
-        c = connection_tree.ReducedConnection(np.eye(3), None)
+        c = connection_tree.OutgoingReducedConnection(np.eye(3), None)
         d = c.copy_with_transform(3.)
 
         assert np.all(d.transform == c.transform*3.)
@@ -57,17 +60,20 @@ class TestReducedConnection(object):
         assert c.keyspace is d.keyspace
 
 
-class TestReducedEnsembleConnection(object):
+class TestOutgoingReducedEnsembleConnection(object):
     def test_eq_no_func_equiv(self):
         # Create some reduced connections and test for equivalence without
         # considering function equivalence.
         eval_points = np.linspace(-1., 1.)
         rcs = [
-            ReducedEnsembleConnection(np.eye(3), None, None, eval_points),
-            ReducedEnsembleConnection(np.eye(3), None, None, eval_points),
-            ReducedEnsembleConnection(np.eye(3), None, None, None),
-            ReducedEnsembleConnection(np.eye(3), None, None, eval_points,
-                                      mock.Mock()),
+            OutgoingReducedEnsembleConnection(np.eye(3), None, None,
+                                              eval_points),
+            OutgoingReducedEnsembleConnection(np.eye(3), None, None,
+                                              eval_points),
+            OutgoingReducedEnsembleConnection(np.eye(3), None, None, None),
+            OutgoingReducedEnsembleConnection(np.eye(3), None, None,
+                                              eval_points, mock.Mock()),
+            OutgoingReducedConnection(np.eye(3), None, None),
         ]
 
         # Define what the equivalences should be
@@ -86,9 +92,12 @@ class TestReducedEnsembleConnection(object):
         # Create some reduced connections and test for equivalence
         eval_points = np.linspace(-1., 1.)
         rcs = [
-            ReducedEnsembleConnection(1., lambda x: x**2, None, eval_points),
-            ReducedEnsembleConnection(1., lambda x: x**2, None, eval_points),
-            ReducedEnsembleConnection(1., lambda x: x**3, None, eval_points),
+            OutgoingReducedEnsembleConnection(1., lambda x: x**2, None,
+                                              eval_points),
+            OutgoingReducedEnsembleConnection(1., lambda x: x**2, None,
+                                              eval_points),
+            OutgoingReducedEnsembleConnection(1., lambda x: x**3, None,
+                                              eval_points),
         ]
 
         # Define what the equivalences should be
@@ -96,6 +105,108 @@ class TestReducedEnsembleConnection(object):
 
         # Define whether the hashes should be equivalent
         hashes = eqs[:]
+
+        # Get the set of indices for equivalence
+        i_s = ((i, j) for i in range(len(rcs)) for j in range(i+1, len(rcs)))
+        for (i, j), eq, he in zip(i_s, eqs, hashes):
+            assert (rcs[i] == rcs[j]) is eq, (i, j)
+            assert (hash(rcs[i]) == hash(rcs[j])) is he, (i, j)
+
+    def test_eq_learning_rules(self):
+        # Create some reduced connections and test for equivalence when
+        # learning rules are present.
+        lrules = [mock.Mock()]
+        rcs = [
+            OutgoingReducedEnsembleConnection(
+                1., None, transmitter_learning_rules=lrules),
+            OutgoingReducedEnsembleConnection(
+                1., None, transmitter_learning_rules=lrules),
+            OutgoingReducedEnsembleConnection(
+                1., None, transmitter_learning_rules=[]),
+        ]
+
+        # Define what the equivalences should be
+        eqs = [False]*(len(rcs))
+
+        # Define whether the hashes should be equivalent
+        hashes = [True] + eqs[1:]
+
+        # Get the set of indices for equivalence
+        i_s = ((i, j) for i in range(len(rcs)) for j in range(i+1, len(rcs)))
+        for (i, j), eq, he in zip(i_s, eqs, hashes):
+            assert (rcs[i] == rcs[j]) is eq, (i, j)
+            assert (hash(rcs[i]) == hash(rcs[j])) is he, (i, j)
+
+
+class TestIncomingReducedConnection(object):
+    def test_eq(self):
+        # Create some incoming reduced connections and test for equivalence.
+        obj_a = mock.Mock()
+        obj_b = mock.Mock()
+        rcs = [
+            IncomingReducedConnection(obj_a, None),
+            IncomingReducedConnection(obj_a, None),  # Same target, same filter
+            IncomingReducedConnection(obj_b, None),  # Diff target, same filter
+            IncomingReducedConnection(obj_a, mock.Mock()),
+            IncomingReducedConnection(obj_b, mock.Mock()),
+        ]
+
+        # Define what the equivalences should be
+        eqs = [True] + [False]*(len(rcs) - 1)
+
+        # Define whether the hashes should be equivalent
+        hashes = eqs[:]
+
+        # Get the set of indices for equivalence
+        i_s = ((i, j) for i in range(len(rcs)) for j in range(i+1, len(rcs)))
+        for (i, j), eq, he in zip(i_s, eqs, hashes):
+            assert (rcs[i] == rcs[j]) is eq, (i, j)
+            assert (hash(rcs[i]) == hash(rcs[j])) is he, (i, j)
+
+
+class TestIncomingReducedEnsembleConnection(object):
+    def test_eq(self):
+        # Create some incoming reduced connections and test for equivalence.
+        obj_a = mock.Mock()
+        obj_b = mock.Mock()
+        rcs = [
+            IncomingReducedEnsembleConnection(obj_a, None),
+            IncomingReducedEnsembleConnection(obj_a, None),
+            IncomingReducedEnsembleConnection(obj_b, None),
+            IncomingReducedEnsembleConnection(obj_a, mock.Mock()),
+            IncomingReducedEnsembleConnection(obj_b, mock.Mock()),
+        ]
+
+        # Define what the equivalences should be
+        eqs = [True] + [False]*(len(rcs) - 1)
+
+        # Define whether the hashes should be equivalent
+        hashes = eqs[:]
+
+        # Get the set of indices for equivalence
+        i_s = ((i, j) for i in range(len(rcs)) for j in range(i+1, len(rcs)))
+        for (i, j), eq, he in zip(i_s, eqs, hashes):
+            assert (rcs[i] == rcs[j]) is eq, (i, j)
+            assert (hash(rcs[i]) == hash(rcs[j])) is he, (i, j)
+
+    def test_eq_with_learning(self):
+        # Create some incoming reduced connections and test for equivalence.
+        obj_a = mock.Mock()
+        lrules = [mock.Mock()]
+        rcs = [
+            IncomingReducedEnsembleConnection(
+                obj_a, None, receiver_learning_rules=lrules),
+            IncomingReducedEnsembleConnection(
+                obj_a, None, receiver_learning_rules=lrules),
+            IncomingReducedEnsembleConnection(
+                obj_a, None)
+        ]
+
+        # Define what the equivalences should be
+        eqs = [False]*len(rcs)
+
+        # Define whether the hashes should be equivalent
+        hashes = [True] + eqs[1:]
 
         # Get the set of indices for equivalence
         i_s = ((i, j) for i in range(len(rcs)) for j in range(i+1, len(rcs)))
