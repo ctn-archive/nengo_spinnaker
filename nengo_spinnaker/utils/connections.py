bb6badaaf67e5c0c7a7d404baedd0809dac6052a
Andrew Mundy <andrew.mundy@ieee.org>
2014-11-07 14:14:32 +0000

    Utility for creating padded transforms.
    Allows construction of matrices that account for padding by pre-slicing.

--- nengo_spinnaker/utils/connections.py
+++ nengo_spinnaker/utils/connections.py
@@ -2,7 +2,9 @@
 """
 
 import copy
+from math import ceil
 import nengo
+import numpy as np
 
 from ..connections.intermediate import IntermediateConnection
 
@@ -36,3 +38,38 @@ def replace_objects_in_connections(connections, replaced_objects):
         new_connections.append(new_c)
 
     return new_connections
+
+
+def get_pre_padded_transform(pre_slice, size_in, transform):
+    """Get a new transform that accounts for slicing applied to the input.
+    """
+    # Get a copy of the transform
+    transform = np.array(transform)
+
+    # If there is no pre-slicing then simply return a copy of the provided
+    # transform.
+    if pre_slice == slice(None) and transform.ndim == 2:
+        return transform
+
+    if transform.ndim < 2:
+        # The given transform is a scalar, so we create an identity matrix of
+        # the correct size which we then pad.
+        if pre_slice == slice(None):
+            size = size_in
+        else:
+            size = int(ceil(
+                (pre_slice.stop - pre_slice.start) /
+                float(1 if pre_slice.step is None else pre_slice.step)
+            ))
+        transform = np.dot(transform, np.eye(size))
+
+    # Otherwise apply the pre-padding
+    if transform.ndim == 2:
+        # The full transform is size_out x size_in:
+        full_transform = np.zeros((transform.shape[0], size_in))
+
+        # Copy in the transform that we already have
+        full_transform[:, pre_slice] = transform
+        return full_transform
+    else:
+        raise ValueError("Transforms with > 2 dims not supported")
--- nengo_spinnaker/utils/tests/test_connections.py
+++ nengo_spinnaker/utils/tests/test_connections.py
@@ -1,6 +1,8 @@
+import numpy as np
 import random
 
 from .. import connections as connections_utils
+from ..connections import get_pre_padded_transform
 
 
 def test_replace_object_in_connections():
@@ -58,3 +60,97 @@ def test_replace_object_in_connections():
             assert nc.pre_obj is replaced_objs[oc.pre_obj]
         if oc.post_obj in replaced_objs:
             assert nc.post_obj is replaced_objs[oc.post_obj]
+
+
+class TestPadTransform(object):
+    """Test conversions from pre-slice and transform into full transform
+    matrices.
+    """
+    def test_non_sliced(self):
+        """Test getting padded matrices when there is no slicing.
+        """
+        pre_slice = slice(None)
+        transform = np.array([[1, 2, 3], [4, 5, 6]])
+        size_in = 3
+
+        # Assert that we just get the transform back
+        new_transform = get_pre_padded_transform(pre_slice, size_in, transform)
+        assert new_transform is not transform
+        assert np.all(new_transform == transform)
+
+    def test_sliced(self):
+        """Test getting padded matrices when there is slicing but a full matrix
+        is provided.
+        """
+        pre_slice = slice(0, 2)
+        transform = np.array([[1, 2], [2, 3], [4, 5]])
+        size_in = 5
+
+        # Assert that the new transform is correct
+        new_transform = get_pre_padded_transform(pre_slice, size_in, transform)
+        assert np.all(
+            new_transform == np.array([[1, 2, 0, 0, 0],
+                                       [2, 3, 0, 0, 0],
+                                       [4, 5, 0, 0, 0]])
+        )
+
+    def test_sliced_discontiguous(self):
+        """Test getting padded matrices when there is slicing but a full matrix
+        is provided.
+        """
+        pre_slice = slice(0, 3, 2)
+        transform = np.array([[1, 2], [2, 3], [4, 5]])
+        size_in = 5
+
+        # Assert that the new transform is correct
+        new_transform = get_pre_padded_transform(pre_slice, size_in, transform)
+        assert np.all(
+            new_transform == np.array([[1, 0, 2, 0, 0],
+                                       [2, 0, 3, 0, 0],
+                                       [4, 0, 5, 0, 0]])
+        )
+
+    def test_scalar(self):
+        """Test getting a matrix when there is no slicing but only a scalar is
+        provided.
+        """
+        pre_slice = slice(None)
+        transform = 3.
+        size_in = 5
+
+        # Assert that the new transform is correct
+        assert np.all(
+            get_pre_padded_transform(pre_slice, size_in, transform) ==
+            np.eye(5) * 3.
+        )
+
+    def test_sliced_scalar(self):
+        """Test getting padded matrices when there is slicing and only a scalar
+        is provided.
+        """
+        pre_slice = slice(2, 4)
+        transform = 3.
+        size_in = 5
+
+        # Assert that the new transform is correct
+        new_transform = get_pre_padded_transform(pre_slice, size_in, transform)
+        assert np.all(
+            new_transform == np.array([[0, 0, 3., 0, 0],
+                                       [0, 0, 0, 3., 0]])
+        )
+
+    def test_sliced_scalar_discontiguous(self):
+        """Test getting padded matrices when there is slicing and only a scalar
+        is provided.
+        """
+        pre_slice = slice(0, 5, 2)
+        transform = 3.
+        size_in = 5
+
+        # Assert that the new transform is correct
+        new_transform = get_pre_padded_transform(pre_slice, size_in, transform)
+        assert np.all(
+            new_transform == np.array([[3., 0., 0., 0., 0.],
+                                       [0., 0., 3., 0., 0.],
+                                       [0., 0., 0., 0., 3.]])
+        )
