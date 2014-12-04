8838154f605bfe790d92434a7dece1630a94f332
Andrew Mundy <andrew.mundy@ieee.org>
2014-12-04 11:32:37 +0000

    Corrections to __init__

--- nengo_spinnaker/__init__.py
+++ nengo_spinnaker/__init__.py
@@ -1,1 +1,6 @@
+from .analyser import Analyser  # noqa: F401
+from .config import Config  # noqa: F401
 from .simulator import Simulator  # noqa: F401
+
+# These need to be imported to register functions with the builder
+from .ensemble import build as ens_build  # noqa: F401
