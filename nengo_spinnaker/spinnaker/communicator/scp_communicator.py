ad1ba9c9d7a9a3296fa00b26e887a2f0acdac9f9
Andrew Mundy <andrew.mundy@ieee.org>
2015-01-19 10:40:03 +0000

    Start SCP Implementation.
     - Read/write
     - LEDs
     - Error handling
     - IPTags

--- nengo_spinnaker/spinnaker/communicator/scp_communicator.py
+++ nengo_spinnaker/spinnaker/communicator/scp_communicator.py
@@ -1,7 +1,9 @@
 """A stop-and-wait blocking implementation of the SCP protocol.
 """
 import collections
+import enum
 import socket
+import struct
 from . import packets
 
 
@@ -10,15 +12,38 @@ CoreInfo = collections.namedtuple(
                 "buffer_size build_date version_string")
 
 
-class SCPTimeoutError(IOError):
-    """Raised when an SCP is not acknowledged within the given period of time.
-    """
-    pass
+class LEDAction(enum.IntEnum):
+    ON = 3
+    OFF = 2
+    TOGGLE = 1
+
+
+class DataType(enum.IntEnum):
+    """Data size types."""
+    BYTE = 0
+    SHORT = 1
+    WORD = 2
+
+
+class SCPCommands(enum.IntEnum):
+    SVER = 0
+    READ = 2
+    WRITE = 3
+    LED = 25
+    IPTAG = 26
+
+
+class IPTagCommands(enum.IntEnum):
+    SET = 1
+    GET = 2
+    CLEAR = 3
 
 
 class SCPCommunicator(object):
     """Implements the SCP protocol for communicating with a SpiNNaker machine.
     """
+    error_codes = {}
+
     def __init__(self, spinnaker_host, n_tries=5, timeout=0.5):
         """Create a new communicator to handle control of the given SpiNNaker
         host.
@@ -44,7 +69,17 @@ class SCPCommunicator(object):
         # The current seq value
         self._seq = 0
 
-    def _send_scp(self, x, y, p, cmd, arg1=0, arg2=0, arg3=0, data=b''):
+    @classmethod
+    def _register_error(cls, cmd_rc):
+        """Register an Exception class as belonging to a certain CMD_RC value.
+        """
+        def err_(err):
+            cls.error_codes[cmd_rc] = err
+            return err
+        return err_
+
+    def _send_scp(self, x, y, p, cmd, arg1=0, arg2=0, arg3=0, data=b'',
+                  expected_args=3):
         """Transmit a packet to the SpiNNaker machine and block until an
         acknowledgement is received.
 
@@ -52,6 +87,9 @@ class SCPCommunicator(object):
         -------
         packet : SCPPacket
             The packet that was received in acknowledgement of the transmitted
+            packet.
+        expected_args : int
+            The number of arguments (0-3) that are expected in the returned
             packet.
         """
         # Construct the packet that will be sent
@@ -81,8 +119,18 @@ class SCPCommunicator(object):
             # the acknowledgement has been returned.
             # Unsure about the padding here
             scp = packets.SCPPacket.from_sdp_packet(
-                packets.SDPPacket.from_bytestring(ack[2:]))
-            
+                packets.SDPPacket.from_bytestring(ack[2:]),
+                n_args=expected_args)
+
+            # Check that the CMD_RC isn't an error
+            if scp.cmd_rc in self.error_codes:
+                raise self.error_codes[scp.cmd_rc](
+                    "Packet with arguments: cmd={}, arg1={}, arg2={}, arg3={};"
+                    " sent to core ({},{},{}) was bad.".format(
+                        cmd, arg1, arg2, arg3, x, y, p
+                    )
+                )
+
             if scp.seq == self._seq:
                 # The packet is the acknowledgement.  Increment the sequence
                 # indicator and return the packet.
@@ -90,7 +138,7 @@ class SCPCommunicator(object):
                 return scp
 
         # The packet we transmitted wasn't acknowledged.
-        raise SCPTimeoutError(
+        raise TimeoutError(
             "Exceeded {} tries when trying to transmit packet.".format(
                 self.n_tries)
         )
@@ -98,7 +146,7 @@ class SCPCommunicator(object):
     def software_version(self, x, y, p):
         """Get the software version for a given SpiNNaker core.
         """
-        sver = self._send_scp(x, y, p, 0)
+        sver = self._send_scp(x, y, p, SCPCommands.SVER)
 
         # Format the result
         # arg1 => p2p address, physical cpu, virtual cpu
@@ -112,3 +160,136 @@ class SCPCommunicator(object):
 
         return CoreInfo(p2p, pcpu, vcpu, version, buffer_size, sver.arg3,
                         sver.data)
+
+    def set_led(self, x, y, led, action=LEDAction.ON):
+        """Set the state of an LED.
+
+        Parameters
+        ----------
+        led : int
+            Number of the LED to set the state of (0-3)
+        action : LEDAction
+            Action to take with the LED (on/off/toggle)
+        """
+        arg1 = int(action) << (led * 2)
+        self._send_scp(x, y, 0, SCPCommands.LED, arg1=arg1, expected_args=0)
+
+    def read(self, x, y, p, address, length_bytes, data_type=DataType.BYTE):
+        """Read a bytestring from an address in memory.
+
+        Parameters
+        ----------
+        address : int
+            The address at which to start reading the data.
+        length_bytes : int
+            The number of bytes to read from memory.
+        data_type : DataType
+            The size of the data to write into memory.
+
+        Returns
+        -------
+        data : bytestring
+            The data is read back from memory as a bytestring.
+        """
+        read_scp = self._send_scp(x, y, p, SCPCommands.READ, address,
+                                  length_bytes, int(data_type),
+                                  expected_args=0)
+        return read_scp.data
+
+    def write(self, x, y, p, address, data, data_type=DataType.BYTE):
+        """Write a bytestring to an address in memory.
+
+        Parameters
+        ----------
+        address : int
+            The address at which to start writing the data.
+        data : bytestring
+            Data to write into memory.
+        data_type : DataType
+            The size of the data to write into memory.
+        """
+        length_bytes = len(data)
+        self._send_scp(x, y, p, SCPCommands.WRITE, address, length_bytes,
+                       int(data_type), data, expected_args=0)
+
+    def iptag_set(self, x, y, iptag, addr, port):
+        """Set the value of an IPTag.
+
+        Parameters
+        ----------
+        iptag : int
+            Index of the IPTag to set
+        """
+        # Format the IP address
+        ip_addr = struct.pack('!4B',
+                              *map(int, socket.gethostbyname(addr).split('.')))
+        self._send_scp(x, y, 0, SCPCommands.IPTAG,
+                       int(IPTagCommands.SET) << 16 | iptag,
+                       port, struct.unpack('<I', ip_addr)[0])
+
+    def iptag_get(self, x, y, iptag):
+        """Get the value of an IPTag.
+
+        Parameters
+        ----------
+        iptag : int
+            Index of the IPTag to get
+        """
+        ack = self._send_scp(x, y, 0, SCPCommands.IPTAG,
+                             int(IPTagCommands.GET) << 16 | iptag, 1,
+                             expected_args=0)
+        return IPTag.from_bytestring(ack.data)
+
+    def iptag_clear(self, x, y, iptag):
+        """Clear an IPTag.
+
+        Parameters
+        ----------
+        iptag : int
+            Index of the IPTag to clear.
+        """
+        self._send_scp(x, y, 0, SCPCommands.IPTAG,
+                       int(IPTagCommands.CLEAR) << 16 | iptag)
+
+class SCPError(IOError):
+    """Base Error for SCP return codes."""
+    pass
+
+
+class TimeoutError(SCPError):
+    """Raised when an SCP is not acknowledged within the given period of time.
+    """
+    pass
+
+
+@SCPCommunicator._register_error(0x81)
+class BadPacketLengthError(SCPError):
+    """Raised when an SCP packet is an incorrect length."""
+    pass
+
+
+@SCPCommunicator._register_error(0x83)
+class InvalidCommandError(SCPError):
+    """Raised when an SCP packet contains an invalid command code."""
+    pass
+
+
+@SCPCommunicator._register_error(0x84)
+class InvalidArgsError(SCPError):
+    """Raised when an SCP packet has an invalid argument."""
+    pass
+
+
+class IPTag(collections.namedtuple("IPTag",
+                                   "addr max port timeout flags count rx_port "
+                                   "spin_addr spin_port")):
+    """An IPTag as read from a SpiNNaker machine."""
+    @classmethod
+    def from_bytestring(cls, bytestring):
+        (ip, max, port, timeout, flags, count, rx_port, spin_addr,
+         spin_port) = struct.unpack("4s 6s 3H I 2H B", bytestring[:25])
+        # Convert the IP address into a string, otherwise save
+        ip_addr = '.'.join(str(x) for x in struct.unpack("4B", ip))
+
+        return cls(ip_addr, max, port, timeout, flags, count, rx_port,
+                   spin_addr, spin_port)
--- nengo_spinnaker/spinnaker/communicator/tests/test_scp_communicator.py
+++ nengo_spinnaker/spinnaker/communicator/tests/test_scp_communicator.py
@@ -1,14 +1,19 @@
 import pytest
+import socket
+import time
 
 from ..scp_communicator import SCPCommunicator
+from .. import scp_communicator
+
+
+@pytest.fixture(scope='module')
+def comms(spinnaker_ip):
+    return SCPCommunicator(spinnaker_ip)
 
 
 @pytest.mark.spinnaker
-def test_sver(spinnaker_ip):
+def test_sver(comms):
     """Test getting the software version data."""
-    # Create an SCPCommunicator for the given SpiNNaker IP address.
-    comms = SCPCommunicator(spinnaker_ip)
-
     # (Assuming a 4-node board) Get the software version for a number of cores.
     for x in range(2):
         for y in range(2):
@@ -17,3 +22,101 @@ def test_sver(spinnaker_ip):
                 assert sver.virt_cpu == p
                 assert "SpiNNaker" in sver.version_string
                 assert sver.version >= 1.3
+
+
+@pytest.mark.spinnaker
+@pytest.mark.parametrize("action", [scp_communicator.LEDAction.ON,
+                                    scp_communicator.LEDAction.OFF,
+                                    scp_communicator.LEDAction.TOGGLE,
+                                    scp_communicator.LEDAction.TOGGLE])
+def test_set_led(comms, action):
+    """Test getting the software version data."""
+    # (Assuming a 4-node board)
+    for x in range(2):
+        for y in range(2):
+            comms.set_led(x, y, 1, action)
+
+    time.sleep(0.05)
+
+
+@pytest.mark.spinnaker
+def test_write_and_read(comms):
+    """Test write and read capabilities by writing a string to SDRAM and then
+    reading back in a different order.
+    """
+    data = b'Hello, SpiNNaker'
+
+    # You put the data in
+    comms.write(0, 0, 0, 0x70000000, data[0:4],
+                scp_communicator.DataType.WORD)
+    comms.write(0, 0, 0, 0x70000004, data[4:6],
+                scp_communicator.DataType.SHORT)
+    comms.write(0, 0, 0, 0x70000006, data[6:],
+                scp_communicator.DataType.BYTE)
+
+    # You take the data out
+    assert comms.read(0, 0, 1, 0x70000000, 1,
+                      scp_communicator.DataType.BYTE) == data[0]
+    assert comms.read(0, 0, 1, 0x70000000, 2,
+                      scp_communicator.DataType.SHORT) == data[0:2]
+    assert comms.read(0, 0, 1, 0x70000000, 4,
+                      scp_communicator.DataType.WORD) == data[0:4]
+
+    # Read out the entire string
+    assert comms.read(0, 0, 1, 0x70000000, len(data),
+                      scp_communicator.DataType.BYTE) == data
+    assert comms.read(0, 0, 1, 0x70000000, len(data),
+                      scp_communicator.DataType.SHORT) == data
+    assert comms.read(0, 0, 1, 0x70000000, len(data),
+                      scp_communicator.DataType.WORD) == data
+
+
+@pytest.mark.spinnaker
+def test_set_get_clear_iptag(comms):
+    # Get our address, then add a new IPTag pointing
+    ip_addr = comms.sock.getsockname()[0]
+    port = 1234
+    iptag = 7
+
+    # Set IPTag 7 with the parameters from above
+    comms.iptag_set(0, 0, iptag, ip_addr, port)
+
+    # Get the IPtag and check that it is as we set it
+    ip_tag = comms.iptag_get(0, 0, iptag)
+    assert ip_addr == ip_tag.addr
+    assert port == ip_tag.port
+    assert ip_tag.flags != 0
+
+    # Clear the IPTag
+    comms.iptag_clear(0, 0, iptag)
+
+    # Check that it is empty by inspecting the flag
+    ip_tag = comms.iptag_get(0, 0, iptag)
+    assert ip_tag.flags == 0
+
+@pytest.mark.spinnaker
+def test_bad_packet_length(comms):
+    """Test transmitting a packet with an incorrect length, this should raise
+    an error.
+    """
+    with pytest.raises(scp_communicator.BadPacketLengthError):
+        comms._send_scp(0, 0, 0, 0, None, None, None, b'')
+
+
+@pytest.mark.spinnaker
+def test_invalid_command(comms):
+    """Test transmitting a packet with an invalid CMD raises an error."""
+    # Create an SCPCommunicator for the given SpiNNaker IP address.
+    with pytest.raises(scp_communicator.InvalidCommandError):
+        comms._send_scp(0, 0, 0, 6)
+
+
+"""
+@pytest.mark.spinnaker
+def test_invalid_argument(spinnaker_ip):
+    # Create an SCPCommunicator for the given SpiNNaker IP address.
+    comms = SCPCommunicator(spinnaker_ip)
+
+    with pytest.raises(scp_communicator.InvalidArgsError):
+        comms._send_scp(0, 0, 0, scp_communicator.SCPCommands.LED, 128)
+"""
